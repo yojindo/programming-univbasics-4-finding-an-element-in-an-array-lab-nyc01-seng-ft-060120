0106986cdc32adb305ea512e6b12e8839d796712
Jake Kwak <jakejaekwak@gmail.com>
2020-05-12 16:39:14 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,8 +5,8 @@ def find_element_index(array, value_to_find)
   
   while count < array.length do
     if array[count] == value_to_find
-      puts array.index
+      array.index
     elsif
-      puts nil
+      nil
   end
 end
